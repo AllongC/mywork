f6475d8344741ccf3ac3451bc0985a019b398273
杨腾祺 <3150475761@qq.com>
2020-06-21 20:38:17 +0800

    08-注册页面布局完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <router-link to="/login">登录</router-link>
+    <router-link to="/register">注册</router-link>
     <router-view></router-view>
   </div>
 </template>
---
+++ src/views/Register.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <CommonSec />
+    <InputSec
+      type="text"
+      InputText="用户名/手机号码"
+      rule="^\d{3,6}$"
+      errMsg="手机号码或用户名格式不正确"
+      @change="setUsername"
+    />
+    <InputSec type="text" InputText="昵称" rule="^\w{3,6}$" errMsg="昵称格式不正确" @change="setNickname" />
+    <InputSec type="password" InputText="密码" rule="^\d{3,6}$" errMsg="密码不正确" @change="setPassword" />
+    <LoginSec btnInfo="注册" @inform="accept" />
+  </div>
+</template>
+
+<script>
+import CommonSec from "@/components/CommonSec";
+import InputSec from "@/components/InputSec";
+import LoginSec from "@/components/LoginSec";
+export default {
+  data() {
+    return {
+      username: "",
+      nickname: "",
+      password: ""
+    };
+  },
+  methods: {
+    accept() {
+      console.log("父级触发...");
+      console.log(this.username);
+      console.log(this.nickname);
+      console.log(this.password);
+    },
+    setUsername(newVal) {
+      this.username = newVal;
+    },
+    setNickname(newVal) {
+      this.nickname = newVal;
+    },
+    setPassword(newVal) {
+      this.password = newVal;
+    }
+  },
+  components: {
+    CommonSec,
+    InputSec,
+    LoginSec
+  }
+};
+</script>
+
+<style lang="less" scoped>
+</style>,
