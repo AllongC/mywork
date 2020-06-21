2ac61accbf81d2882b47e86fa39ced51f8e6a9a2
杨腾祺 <3150475761@qq.com>
2020-06-21 19:53:02 +0800

    03-封装输入框组件

---
+++ src/components/InputSec.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="InputSec">
+    <input :type="type" :placeholder="InputText" v-model="EditVal" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      EditVal: "",
+      flag: true
+    };
+  },
+  props: ["type", "InputText", "rule", "errMsg"],
+  watch: {
+    EditVal(newVal) {
+      const reg = new RegExp(this.rule).test(newVal);
+      this.flag = reg;
+      if (!this.flag) {
+        console.log(this.errMsg);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.InputSec {
+  padding: 0vw 5.556vw 0vw 5.556vw;
+  input {
+    outline: none;
+    height: 16.667vw;
+    width: 100%;
+    border: none;
+    border-bottom: 1px solid black;
+    font-size: 5vw;
+  }
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,12 +6,8 @@
     <div class="logoSec">
       <span class="iconfont iconnew"></span>
     </div>
-    <div class="InputSec">
-      <input type="text" />
-    </div>
-    <div class="InputSec">
-      <input type="text" />
-    </div>
+    <InputSec type="text" InputText="用户名/手机号码" rule="^\d{3,6}$" errMsg="手机号码格式不正确" />
+    <InputSec type="password" InputText="密码" rule="^\d{3,6}$" errMsg="密码格式不正确" />
     <div class="LoginSec">
       <button>登录</button>
     </div>
@@ -19,7 +15,12 @@
 </template>
 
 <script>
-export default {};
+import InputSec from "@/components/InputSec";
+export default {
+  components: {
+    InputSec
+  }
+};
 </script>
 
 <style lang="less" scoped>
@@ -36,19 +37,11 @@ export default {};
     font-size: 35vw;
   }
 }
-.InputSec {
-  padding: 0vw 5.556vw 0vw 5.556vw;
-  input {
-    height: 16.667vw;
-    width: 100%;
-    border: none;
-    border-bottom: 1px solid black;
-  }
-}
 .LoginSec {
   margin-top: 16.667vw;
   padding: 0vw 5.556vw 0vw 5.556vw;
   button {
+    outline: none;
     border: none;
     background-color: #d81e06;
     color: white;
