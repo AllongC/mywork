59b48e4047888ac2deb7dd4a8d764c37166ea543
杨腾祺 <3150475761@qq.com>
2020-06-27 17:20:24 +0800

    32-主页栏目完成

---
+++ src/components/HomeTab.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <van-tabs :v-model="active">
+      <van-tab v-for="item in categoryList" :key="item.id" :title="item.name">内容 1</van-tab>
+    </van-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["active", "categoryList"]
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,31 @@
 <template>
   <div>
     <HomeTop />
+    <HomeTab :active="active" :categoryList="categoryList" />
   </div>
 </template>
 
 <script>
 import HomeTop from "@/components/HomeTop";
+import HomeTab from "@/components/HomeTab";
 export default {
+  data() {
+    return {
+      active: 0,
+      categoryList: []
+    };
+  },
   components: {
-    HomeTop
+    HomeTop,
+    HomeTab
+  },
+  mounted() {
+    this.$axios({
+      url: "/category",
+      method: "get"
+    }).then(res => {
+      this.categoryList = res.data.data;
+    });
   }
 };
 </script>
