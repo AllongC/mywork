fb970464887fd45a73a1e91e943e24e387150dbd
杨腾祺 <3150475761@qq.com>
2020-06-27 16:44:56 +0800

    30-取消关注功能

--- src/views/personal/Focus.vue
+++ src/views/personal/Focus.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
     <CommonTop topVal="我的关注" />
-    <div class="item" v-for="list in userList" :key="list.id">
-      <img v-if="list.head_img" :src="$axios.defaults.baseURL + list.head_img" />
-      <img v-else src="@/img/logo.jpg" />
-      <div class="foucsInfo">
-        <p>{{list.nickname}}</p>
-        <p class="focusData">{{list.create_date.split("T")[0]}}</p>
+    <div v-if="userList.length">
+      <div class="item" v-for="list in userList" :key="list.id">
+        <img v-if="list.head_img" :src="$axios.defaults.baseURL + list.head_img" />
+        <img v-else src="@/img/logo.jpg" />
+        <div class="foucsInfo">
+          <p>{{list.nickname}}</p>
+          <p class="focusData">{{list.create_date.split("T")[0]}}</p>
+        </div>
+        <button v-if="intermit.indexOf(list.id)==-1" @click="unfollow(list.id)">取消关注</button>
+        <button v-else @click="follow(list.id)" class="redBtn">关注</button>
       </div>
-      <button>取消关注</button>
+    </div>
+    <div v-else class="noFocus">
+      <p>暂未关注</p>
     </div>
   </div>
 </template>
@@ -18,18 +24,48 @@ import CommonTop from "@/components/CommonTop";
 export default {
   data() {
     return {
-      userList: []
+      userList: [],
+      intermit: []
     };
+  },
+  methods: {
+    upData() {
+      this.$axios({
+        url: "/user_follows",
+        method: "get"
+      }).then(res => {
+        this.userList = res.data.data;
+      });
+    },
+    unfollow(id) {
+      this.intermit.push(id);
+    },
+    follow(id) {
+      const newArr = this.intermit.map((item, index) => {
+        if (item !== id) {
+          return item;
+        }
+      });
+      this.intermit = newArr;
+    }
   },
   components: {
     CommonTop
   },
-  mounted() {
-    this.$axios({
-      url: "/user_follows",
-      method: "get"
-    }).then(res => {
-      this.userList = res.data.data;
+  created() {
+    this.upData();
+  },
+  destroyed() {
+    this.intermit.forEach(item => {
+      this.$axios({
+        url: "/user_unfollow/" + item,
+        method: "get"
+      }).then(res => {
+        const { message } = res.data;
+        if (message == "取消关注成功") {
+          this.upData();
+        }
+      });
     });
   }
 };
@@ -67,6 +103,19 @@ export default {
     border: none;
     border-radius: 15px;
     font-size: 12px;
+  }
+  .redBtn {
+    background-color: red;
+    color: #fff;
+  }
+}
+.noFocus {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  p {
+    margin-top: 50%;
+    color: #666;
   }
 }
 </style>
