038ed017d1456b6543cf9ae87a6321c5afb70a65
杨腾祺 <3150475761@qq.com>
2020-07-04 15:55:31 +0800

    55-更多跟帖完善

--- src/views/MoreComment.vue
+++ src/views/MoreComment.vue
@@ -1,32 +1,48 @@
 <template>
   <div>
     <CommonTop topVal="更多跟贴" />
-    <comment :comment="item" v-for="item in commentList " :key="item.id" />
+    <comment :comment="item" v-for="item in commentList " :key="item.id" @sendIndexId="acceptId" />
+    <Input :lastId="lastId" ref="Input" @reload="load" />
   </div>
 </template>
 
 <script>
 import CommonTop from "@/components/CommonTop";
 import comment from "@/components/comment/index";
+import Input from "@/components/comment/CommentInput";
 export default {
   data() {
     return {
-      commentList: []
+      commentList: [],
+      lastId: ""
     };
   },
+  methods: {
+    acceptId(id) {
+      this.lastId = id;
+      this.$refs.Input.getFocus();
+    },
+    load() {
+      this.$axios({
+        url: "/post_comment/" + this.$route.params.id,
+        method: "get"
+      }).then(res => {
+        const { data } = res.data;
+        if (data.length >= 3) {
+          data.length = 3;
+        }
+        this.commentList = data;
+        console.log(this.commentList);
+      });
+    }
+  },
   mounted() {
-    this.$axios({
-      url: "/post_comment/1",
-      method: "get"
-    }).then(res => {
-      const { data } = res.data;
-      this.commentList = data;
-      console.log(this.commentList);
-    });
+    this.load();
   },
   components: {
     CommonTop,
-    comment
+    comment,
+    Input
   }
 };
 </script>
